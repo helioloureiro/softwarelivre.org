cbb60983a8db7122a468f8819509247a89d3fef0
Helio Loureiro <helio@loureiro.eng.br>
2020-09-12 13:26:39 +0200

    Adicionando estrutura de diretorios furusho

---
+++ furusho/blog/console-colorido-usando-node.js
@@ -0,0 +1,183 @@
+
+<div id="article" class="text-article">
+
+
+
+<script type="text/javascript">
+    window.ONE_COMMENT = "One comment";
+    window.COMMENT_PLURAL = "comments";
+    window.NO_COMMENT_YET = "No comments yet";
+</script>
+
+  <div id="article-parent">
+    <a class="button icon-back with-text" title="Go back to FREE SOFTWAR..." href="http://softwarelivre.org/furusho/blog">&nbsp;<span>Go back to FREE SOFTWAR...</span></a>
+  </div>
+
+<div id="article-toolbar">
+  <div>
+  <div id="article-actions">
+
+    
+      <script>fullscreenPageLoad('#article')</script>
+    <a id="fullscreen-btn" onClick="toggle_fullwidth(&#39;#article&#39;)" class="button with-text icon-fullscreen" href="#" title="Go to full screen mode"><span>Full screen</span></a><a style="display: none;" id="exit-fullscreen-btn" onClick="toggle_fullwidth(&#39;#article&#39;)" class="button with-text icon-fullscreen" href="#" title="Exit full screen mode"><span>Exit full screen</span></a>
+
+
+
+
+
+
+
+
+
+    
+
+    
+
+  </div>
+  <div id="article-header">
+    
+    
+      <h1 class="title">
+      Console colorido usando Node.js
+  </h1>
+  <span class="publishing-info">
+  <span class="date">
+    Luglio 31, 2012 0:00
+  </span>
+  <span class="author">
+    , by Software Livre Brasil
+  </span>
+  <span class="comments">
+     - <a href="/furusho/blog/console-colorido-usando-node.js#comments_list"><span class='comment-count hide'>0</span><span class='comment-count-write-out'>no comments yet</span></a>
+  </span>
+
+<span class="followers-count">
+|
+    No one following this article yet.
+</span>
+
+</span>
+
+  <div id='article-sub-header'>
+      <div id="article-hits">
+        Viewed 102 times 
+      </div>
+
+  </div>
+
+
+    
+  </div>
+</div>
+
+</div>
+
+
+
+  <div id="addThis">
+  <script type="text/javascript">
+    addthis_pub = 'ra-4d93d22a7a91a05f';
+    addthis_logo = '';
+    addthis_options = 'identica, twitter, www.facebook.com, favorites, email, delicious, tumblr, digg, slashdot, more';
+  </script>
+  <a href="http://www.addthis.com/bookmark.php" id="bt_addThis" target="_blank" onmouseover="return addthis_open(this, '', '[URL]')" onmouseout="addthis_close()" onclick="return addthis_sendto()"><img width="53" height="16" border="0" alt="" src="/images/bt-bookmark.gif" /></a>
+</div>
+
+
+  <div class="article-body article-body-text-article">
+    <p>\033[1;33m\033[41mBoa dica!\033[0m</p> <p>Enviado por Thiago Avelino (thiagoavelinosterΘgmail·com):</p> <div class="usertext">“Para quem esta acostumado trabalhar com make para subir um ambiente ou o pessoal que gosta de escrever alguns shell scripts para automação de simples rotinas acho muito importante colocar cor nos retornos do script, isso é ótimo para sinalizar o que realmente o software (ou script) esta fazendo. Para deixar um retorno colorido usando Node.js não é muito diferente de shell script, pois o que vai processar o retorno é um console bash (zsh ou qual quer outro shell)…” [<a href="http://avelino.us/2012/07/27/console-colorido-usando-node-js/">referência: avelino.us</a>]</div> <img src="http://br.linux.feedsportal.com/c/34277/f/623547/s/21e50923/mf.gif" border="0" height="1" width="1"><div class="mf-viral"><table border="0"><tr>
+<td><a href="http://share.feedsportal.com/viral/sendEmail.cfm?lang=pt&amp;title=Console+colorido+usando+Node.js&amp;link=http%3A%2F%2Fbr-linux.org%2F2012%2Fconsole-colorido-usando-node-js%2F" target="_blank"><img src="http://res3.feedsportal.com/images/emailthis2_pt.gif" border="0"></a></td>
+<td><a href="http://res.feedsportal.com/viral/bookmark_pt.cfm?title=Console+colorido+usando+Node.js&amp;link=http%3A%2F%2Fbr-linux.org%2F2012%2Fconsole-colorido-usando-node-js%2F" target="_blank"><img src="http://rss.feedsportal.com/images/bookmark.gif" border="0"></a></td>
+</tr></table></div><br><br><a href="http://da.feedsportal.com/r/139262735109/u/0/f/623547/c/34277/s/21e50923/a2.htm"><img src="http://da.feedsportal.com/r/139262735109/u/0/f/623547/c/34277/s/21e50923/a2.img" border="0"></a><img src="http://pi.feedsportal.com/r/139262735109/u/0/f/623547/c/34277/s/21e50923/a2t.img" border="0" height="1" width="1">
+    <br style="clear:both" />
+  </div> <!-- end class="article-body" -->
+
+
+
+<div id="article-source">Source: <a href="http://br.linux.feedsportal.com/c/34277/f/623547/s/21e50923/l/0Lbr0Elinux0Borg0C20A120Cconsole0Ecolorido0Eusando0Enode0Ejs0C/story01.htm">http://br.linux.feedsportal.com/c/34277/f/623547/s/21e50923/l/0Lbr0Elinux0Borg0C20A120Cconsole0Ecolorido0Eusando0Enode0Ejs0C/story01.htm</a></div>
+
+
+
+  <div class="comments" id="comments_list">
+    <h3 class="no-comments-yet">
+      <span class='comment-count hide'>0</span><span class='comment-count-write-out'>no comments yet</span>
+    </h3>
+
+
+    <ul class="article-comments-list">
+    </ul>
+
+      <div id='page-comment-form' class='page-comment-form'><div class="page-comment-form">
+
+
+
+<div class="post_comment_box closed">
+
+  <a class="display-comment-form" href="#">Post a comment</a>
+<form class="comment_form" id="new_comment" onsubmit="jQuery.ajax({data:jQuery.param(jQuery(this).serializeArray()) + &#39;&amp;authenticity_token=&#39; + encodeURIComponent(&#39;/vMgzZYYTd/4a21bKfrt6PgwoaYppsH72apIKcy8UEpjm5DM5ENPPBdwmMWvepeLt6FrZSEqBqmEJnEtkzf2mA==&#39;), dataType:&#39;script&#39;, type:&#39;post&#39;, url:&#39;/profile/furusho/comment/create/458645?escape=false&#39;}); return false;" action="/profile/furusho/comment/create/458645" accept-charset="UTF-8" method="post"><input name="utf8" type="hidden" value="&#x2713;" /><input type="hidden" name="authenticity_token" value="YVx3bMsN2Ez5WyJDp5wcPoZnPDmZ9bPdjbuZvkbJSz38NMdtuVbarxZA190hHGZdyfb2+pF5dI/QN6C6GULt7w==" />
+
+  <p><span class="required-field">The <label class='pseudoformlabel'>highlighted</label> fields are mandatory.</span></p>
+
+
+    <span class="required-field"><div class="formfieldline"><label class="formlabel" for="comment_name">Nome</label><div class="formfield type-text"><input type="text" name="comment[name]" id="comment_name" /></div></div></span>
+    <span class="required-field"><div class="formfieldline"><label class="formlabel" for="comment_email">e-mail</label><div class="formfield type-text"><input type="text" name="comment[email]" id="comment_email" /></div></div></span>
+    <p>
+    If you are a registered user, you can login and be automatically recognized.
+    </p>
+
+
+  <div class="formfieldline"><label class="formlabel" for="comment_title">Titolo</label><div class="formfield type-text"><input type="text" name="comment[title]" id="comment_title" /></div></div>
+  <span class="required-field"><div class="formfieldline"><label class="formlabel" for="comment_body">Enter your comment</label><div class="formfield type-textarea"><textarea rows="5" name="comment[body]" id="comment_body">
+</textarea></div></div></span>
+
+    <p><script src="https://www.google.com/recaptcha/api.js" async defer></script>
+<div class="g-recaptcha " data-sitekey="6LcK7CYUAAAAABl-eMkoFIv3VvOOIPwiXeinFmUi" ajax="true"></div>
+          <noscript>
+            <div>
+              <div style="width: 302px; height: 422px; position: relative;">
+                <div style="width: 302px; height: 422px; position: absolute;">
+                  <iframe
+                    src="https://www.google.com/recaptcha/api/fallback?k=6LcK7CYUAAAAABl-eMkoFIv3VvOOIPwiXeinFmUi"
+                    frameborder="0" scrolling="no"
+                    style="width: 302px; height:422px; border-style: none;">
+                  </iframe>
+                </div>
+              </div>
+              <div style="width: 300px; height: 60px; border-style: none;
+                bottom: 12px; left: 25px; margin: 0px; padding: 0px; right: 25px;
+                background: #f9f9f9; border: 1px solid #c1c1c1; border-radius: 3px;">
+                <textarea id="g-recaptcha-response" name="g-recaptcha-response"
+                  class="g-recaptcha-response"
+                  style="width: 250px; height: 40px; border: 1px solid #c1c1c1;
+                  margin: 10px 25px; padding: 0px; resize: none;" value="">
+                </textarea>
+              </div>
+            </div>
+          </noscript>
+</p>
+
+
+  <input type="hidden" name="confirm" id="confirm" value="false" />
+  <input type="hidden" name="view" id="view" />
+  <input type="hidden" name="comment[reply_of_id]" id="comment_reply_of_id" />
+
+  
+
+  <div class=" button-bar">
+    <input type="submit" name="commit" value="Post comment" onclick="save_comment(this); return false;" class="button with-text icon-add submit" />
+      <a id="cancel-comment" class="button icon-cancel with-text" title="Cancel" href="">&nbsp;<span>Cancel</span></a>
+<br style="clear: left;" /></div></form>
+
+</div><!-- end class="post_comment_box" -->
+</div><!-- end class="page-comment-form" -->
+
+<script src="/assets/comment_form-6675ef0f0fa17bef4ca4a3b992647bd9.js"></script>
+</div>
+  </div><!-- end class="comments" -->
+
+</div><!-- end id="article" -->
+<link rel="stylesheet" media="screen" href="/assets/vendor/jquery.fancybox-f18bccba6b2b1108ccca4462da78aa43.css" /><script src="/assets/vendor/jquery.fancybox.pack-c84445b4cd8162f8a15703d690ccc514.js"></script><script>
+//<![CDATA[
+apply_zoom_to_images("Zoom in")
+//]]>
+</script>
